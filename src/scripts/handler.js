62befb0a7ea99ef9e055ddee3317ef82cf821862
AnthonyTB <Anthonylol@protonmail.com>
2019-11-05 14:33:33 -0800

    added submit, cancel submit button functionality

--- src/scripts/handler.js
+++ src/scripts/handler.js
@@ -17,7 +17,7 @@ const defaultView = function() {
 const generateBookmark = function(bookmark) {
   return `
     <li class="js-bookmark" data-item-id="${bookmark.id}">
-      <h3>${bookmark.title}</h3><span>${bookmark.rating}</span>
+      <h3>${bookmark.title}</h3><span>${bookmark.rating}<i class="fas fa-star"></i></span>
     </li>`;
 };
 
@@ -37,7 +37,26 @@ const render = function() {
     bookmarkString = `<div><h5>${store.error}</h5></div>` + bookmarkString;
   }
   // insert that HTML into the DOM
-  $('main').html(bookmarkString);
+  $('.results-list').html(bookmarkString);
+};
+
+const addNewString = function() {
+  $('.creator').html(`
+  <form class='inputs'>
+    <label for='nameInput'>Name</label>
+        <input type='text' name='nameInput' class='nameInput' placeholder='Google' required></input>
+    <label for='urlInput'>Site Url</label>
+        <input type='text' name='urlInput' class='urlInput' placeholder='https://Google.com' required></input>
+    <label for='ratingInput'>Rating</label>
+        <input type='number' name='ratingInput' class='ratingInput' placeholder='1-5' required></input>
+    <label for='descInput'>Description</label>
+        <input type='text' name='descInput' class='descInput' placeholder='Google is a search engine'></input>
+  </form>
+  <div class='buttons-row'>
+    <button class="complete"><i class="fas fa-plus"></i></button>
+    <button class="cancel"><i class="fas fa-times"></i></button>
+  </div>
+  `);
 };
 
 // add new button functionality
@@ -45,13 +64,21 @@ const addNewHandler = function() {
   $('.add-new').on('click', e => {
     e.preventDefault();
     console.log('create works');
+    addNewString();
   });
 };
 
 const createButton = function() {
-  $('.createItem').submit('.pushItem', e => {
+  $('.creator').on('click', '.complete', e => {
     e.preventDefault();
     console.log('submit works');
+  });
+};
+
+const cancelCreateButton = function() {
+  $('.creator').on('click', '.cancel', e => {
+    e.preventDefault();
+    console.log('cancel submit works');
   });
 };
 
@@ -59,6 +86,7 @@ const createButton = function() {
 const bindEventListeners = function() {
   addNewHandler();
   createButton();
+  cancelCreateButton();
 };
 
 // exports
